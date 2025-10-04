c46efe1e6a7999c61e81193cd4ae5c6e0d26a41b
Salomo Hendrian Sudjono <95460454+SalomoHS@users.noreply.github.com>
2025-10-04 23:45:03 +0700

    Update route.ts

--- app/api/admin/logout/route.ts
+++ app/api/admin/logout/route.ts
@@ -4,5 +4,5 @@ import { getAdminCookieName } from "@/lib/auth"
 
 export async function POST() {
   cookies().delete(getAdminCookieName())
-  return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_URL),{ status: 303 })
+  return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_URL))
 }
